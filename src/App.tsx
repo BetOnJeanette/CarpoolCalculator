f20a0bf5f889ff68d5273ef8e41d710518fac4d8
Jeanette Crull <jeanetteCrull99@gmail.com>
2024-09-08 19:40:44 -0400

    Create a submit button for the destination

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,21 @@ import { createContext, createSignal, onMount, type Component } from 'solid-js';
 import { Destination } from "./pages/Destination"
 import styles from './App.module.css';
 import { AppContextProvider } from './AppContext';
+import { FeatureResponse } from './classes/FeatureResponse';
 
 const App: Component = () => {
+
+  const [destination, setDestination] = createSignal<FeatureResponse>();
+  
+  function updateDestination(newDest: FeatureResponse) {
+    setDestination(newDest);
+  }
+
   return (
     <AppContextProvider>
       <div class={styles.App}>
         <header class={styles.mainBody}>
-          <Destination />
+          <Destination onSubmitDest={updateDestination}/>
         </header>
       </div>
     </AppContextProvider>
--- src/pages/Destination.tsx
+++ src/pages/Destination.tsx
@@ -3,14 +3,27 @@ import {Select, createAsyncOptions} from "@thisbeyond/solid-select"
 import AddressPicker from "../components/AddressPicker/AddressPicker";
 import { useAppContext } from "../AppContext";
 import { FeatureResponse } from "../classes/FeatureResponse";
+import { Button } from "@suid/material";
+import { SubmitButton } from "../components/submitButton/SubmitButton";
 
-const Destination: Component = () => {
+interface IDestinationProps {
+    onSubmitDest: (dest: FeatureResponse) => void
+}
+
+const Destination: Component<IDestinationProps> = ({onSubmitDest}: IDestinationProps) => {
     const [dest, setDest] = createSignal<FeatureResponse>()
     const contextData = useAppContext();
     if (contextData === null ) throw new Error();
+    
+    const submitDestination = () => {
+        const chosenDest = dest()
+        if (chosenDest === undefined) throw new Error("No destination chosen")
+        onSubmitDest(chosenDest)
+    }
 
     return <div>
         <AddressPicker updateAddress={setDest}/>
+        <SubmitButton onSubmit={submitDestination}></SubmitButton>
     </div>
 }
 
