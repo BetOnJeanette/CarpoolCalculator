6ea49c1516e660b297d879585ac5a262ecf5bc80
Jeanette Crull <jeanetteCrull99@gmail.com>
2024-09-11 17:21:53 -0400

    Create page for Groups

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,54 @@
-import { createSignal, Component } from 'solid-js';
+import { createSignal, Component, onMount, lazy, JSX, ValidComponent } from 'solid-js';
 import { Destination } from "./pages/Destination/Destination"
 import styles from './App.module.css';
 import { AppContextProvider } from './AppContext';
 import { SelectableLocation } from './classes/Location';
+import { Group } from './classes/Group';
+import { Dynamic } from 'solid-js/web';
+
+const GroupsPage = lazy(async () => await import("./pages/Groups/Groups"))
+
+enum States {
+  Destination,
+  Groups,
+  Cars,
+  Calculating,
+  Route
+}
 
 const App: Component = () => {
+  const [state, setState] = createSignal<States>(States.Destination)
+  const defaultState = () => Destination({onSubmitDest: updateDestination});
 
-  const [destination, setDestination] = createSignal<SelectableLocation>();
+  let destination: SelectableLocation;
+  let groups: Group[];
   
-  function updateDestination(newDest: SelectableLocation) {
-    setDestination(newDest);
+  function updateDestination(newDest: SelectableLocation){
+    destination = newDest;
+    setState(States.Groups)
   }
+
+  function updateGroups(newGroups: Group[]){
+    groups = newGroups
+  }
+
+  function GoBack(){
+    setState(state() - 1)
+  }
+
+  const StateMap = new Map<States, () => JSX.Element>()
+  StateMap.set(States.Destination, defaultState)
+  
+  onMount(() => {
+    States.Groups, StateMap.set(States.Groups, () => GroupsPage({BackUp: GoBack, UpdateGroups: updateGroups}));
+  })
 
   return (
     <AppContextProvider>
       <div class={styles.App}>
-        <header class={styles.mainBody}>
-          <Destination onSubmitDest={updateDestination}/>
-        </header>
+        <main class={styles.mainBody}>
+          <Dynamic component={StateMap.get(state()) as ValidComponent || defaultState as ValidComponent}></Dynamic>
+        </main>
       </div>
     </AppContextProvider>
   );
---
+++ src/pages/Groups/Groups.tsx
@@ -0,0 +1,41 @@
+import { createSignal, For, JSX } from "solid-js";
+import { Group } from "../../classes/Group";
+import { Accordion } from "@kobalte/core/accordion";
+import { GroupCollapsible } from "../../components/Group/GroupCollapsible";
+
+interface IGroupsPageProps {
+    BackUp(): void
+    UpdateGroups(groups: Group[]): void
+}
+
+export default function GroupsPage({BackUp: GoBack, UpdateGroups}: IGroupsPageProps){
+    let lastKey = 0;
+    const [currentKey, setcurrentKey] = createSignal<number>(lastKey);
+    const GroupList: Array<JSX.Element> = [];
+
+    const groups = new Map<number, Group>()
+
+
+    function AddGroup() {
+        const groupKey = lastKey;
+        const newGroupsElement = GroupCollapsible({
+            UpdateGroup: groups.set,
+            RemoveGroup: groups.delete,
+            CurrentActiveKey: currentKey,
+            SetAsOpen: setcurrentKey,
+            key: groupKey
+        })
+        lastKey++;
+        GroupList.push(newGroupsElement)
+    }
+
+    function onSubmit() {
+        UpdateGroups(Array.from(groups.values()))
+    }
+    AddGroup()
+    return (
+        <div class="Groups">
+            <For each={GroupList}>{(item, index) => item}</For>
+        </div>
+    )
+}
