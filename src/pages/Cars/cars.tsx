d722c5f899e1380840ddafbb681f592ff85863d5
Jeanette Crull <jeanetteCrull99@gmail.com>
2024-09-19 18:00:53 -0400

    Create screen for cars Fixes #3

---
+++ src/pages/Cars/cars.tsx
@@ -0,0 +1,10 @@
+import { JSX } from "solid-js";
+import styles from "cars.module.css"
+
+export default function():JSX.Element {
+    return (
+        <div class={styles.carsPage}> 
+    
+        </div>
+    )
+}
