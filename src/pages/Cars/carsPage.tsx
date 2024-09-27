1986d3c651db595f3f742197b9ce172e98076758
Jeanette Crull <jeanetteCrull99@gmail.com>
2024-09-27 10:20:41 -0400

    Render using array of cars instead of array of UI elements

--- src/pages/Cars/carsPage.tsx
+++ src/pages/Cars/carsPage.tsx
@@ -11,31 +11,25 @@ interface ICarsProps {
     onBack(): void
 }
 
-interface IControlGroup {
-    UI: JSX.Element
-    Car?: Car
-}
-
 export default function CarsPage({availableGroups, onSubmit, onBack}: ICarsProps):JSX.Element {
-    const [cars, setCars] = createSignal<IControlGroup[]>([])
+    const [cars, setCars] = createSignal<Array<Car | undefined>>([])
     
     function AddCar(){
-        const newGroup:IControlGroup = {
-            UI: (<CarCollapsible availableGroups={availableGroups} onChange={(val) => newGroup.Car = val}/>)
-        }
-        setCars([...cars(), newGroup])
+        setCars([...cars(), undefined])
     }
     
     function AttemptSubmit(){
-        const unfinishedIdx = cars().findIndex((val) => val.Car === undefined);
+        const unfinishedIdx = cars().findIndex((val) => val === undefined);
         if (unfinishedIdx !== -1) throw new Error(`At least one is not finished: ${unfinishedIdx}`)
-        onSubmit(cars().map(val => val.Car).filter(val => val !== undefined))
+        onSubmit(cars().filter(val => val !== undefined))
     }
+
+    AddCar()
 
     return (
         <div class={styles.carsPage}>
             <div class={styles.cars}>
-                <For each={cars()}>{(item) => item.UI}</For>
+                <For each={cars()}>{(item) => <CarCollapsible existingCar={item} onChange={newItem => item = newItem} availableGroups={availableGroups}/>}</For>
             </div>
             <button class={[styles.backButton, "button"].join(" ")} onClick={onBack}>Back</button>
             <button class={[styles.addButton, "button"].join(" ")} onClick={AddCar}>Add car</button>
