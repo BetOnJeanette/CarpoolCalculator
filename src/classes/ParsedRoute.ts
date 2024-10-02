291ea4b11fb34d6d32df9c7daf3b6ecf4fbb8e8f
Jeanette Crull <jeanetteCrull99@gmail.com>
2024-10-02 15:59:21 -0400

    Create parsed route class

---
+++ src/classes/ParsedRoute.ts
@@ -0,0 +1,14 @@
+import { Car } from "./Car";
+import { Group } from "./Group";
+import { IRoute } from "./Routes";
+import { StepType } from "./RouteStep";
+
+export class ParsedRoute {
+    public Vehicle: Car;
+    public PicksUp: Group[];
+
+    public constructor(route: IRoute, availableCars: Car[], availableGroups: Group[]){
+        this.Vehicle = availableCars[route.vehicle];
+        this.PicksUp = route.steps.filter(val => val.type === StepType.job).map(val => availableGroups[val.id])
+    }
+}
